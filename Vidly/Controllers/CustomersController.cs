250099d2da9075fb0cbeb2d4fb1469d9f0165c14
DESKTOP-A034AQG\qtrun <qtrung1305@gmail.com>
2018-06-28 11:18:57 +0700

    Exercise 1 in course Working with Data

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        // ctor
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // override Dispose
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //var customers = new List<Customer>
+            //{
+            //    new Customer {Name = "Trung Vo"},
+            //    new Customer {Name = "Van Nguyen"}
+            //};
+
+            //var customerViewModel = new CustomerViewModel
+            //{
+            //    Customers = customers
+            //};
+
+            //return View(customerViewModel);
+
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Trung Vo"},
+                new Customer {Name = "Van Nguyen"}
+            };
+
+            if (id <= customers.Count)
+            {
+                return Content(customers[id - 1].Name);
+            }
+            else
+            {
+                return Content("Have no Customer with this Id");
+            }
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,32 +1,58 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        private ApplicationDbContext _context;
+
+        // ctor
+        public MoviesController()
         {
-            var movie = new Movie() { Name = "Game of Thrones" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-
-            return View(viewModel);
-
+            _context = new ApplicationDbContext();
         }
+
+
+        [Route("movies")]
+        public ActionResult Index()
+        {
+            //var movies = new List<Movie>
+            //{
+            //    new Movie { Name = "Game of Thrones" },
+            //    new Movie { Name = "Breaking bad" }
+            //};
+
+            //var movieViewModel = new MovieViewModel
+            //{
+            //    Movies = movies
+            //};
+            //return View(movieViewModel);
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        // GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Game of Thrones" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2" }
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+
+        //    return View(viewModel);
+
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
